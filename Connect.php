07ddefcc55dcbce2cbe1a47865c82f29231a4208
Rolands <rolands_sapoznikovs@inbox.lv>
2022-11-28 17:28:32 +0200

    Fixing php

--- Connect.php
+++ Connect.php
@@ -5,16 +5,16 @@
   $Password = $_POST['Password'];
 
 
-  $conn = new mysqli('localhost','root','','FruitClicker')
-if($conn->connect_error)
-    die('Connection failed : '.$conn->connect_error);
-}else{
-  $stmt = $conn->prepare("Insert into Registration(FirstName, LastName, Email, Password)
-    values(?, ?, ?, ?)");
-  %stmt->bind_param("ssss",$FirstName, $LastName, $Email, $Password);
-  $stmt->execute();
-  echo "Registration Successfully";
-  $stmt->close();
-  $conn->close();
-}
+  $conn = new mysqli('localhost','root','','fruitclicker');
+  if($conn->connect_error){
+      die('Connection failed : '.$conn->connect_error);
+  }else{
+    $stmt = $conn->prepare("Insert into registration(FirstName, LastName, Email, Password)
+      values(?, ?, ?, ?)");
+    $stmt->bind_param("ssss",$FirstName, $LastName, $Email, $Password);
+    $stmt->execute();
+    echo "registration Successfully";
+    $stmt->close();
+    $conn->close();
+       }
 ?>
