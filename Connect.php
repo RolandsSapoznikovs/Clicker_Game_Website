694d80fc3c62743dda1e8ea6c0ac706583a5f107
Rolands Sapoznikovs <78017808+RolandsSapoznikovs@users.noreply.github.com>
2022-12-05 10:45:43 +0000

    Adding css and html

--- Connect.php
+++ Connect.php
@@ -1,17 +1,16 @@
 <?php
-  $FirstName = $_POST['FirstName'];
-  $LastName = $_POST['LastName'];
-  $Email = $_POST['Email'];
-  $Password = $_POST['Password'];
+  $UserName = $_POST['userinp'];
+  $Email = $_POST['email'];
+  $Password = $_POST['parolinp'];
 
 
   $conn = new mysqli('localhost','root','','fruitclicker');
   if($conn->connect_error){
       die('Connection failed : '.$conn->connect_error);
   }else{
-    $stmt = $conn->prepare("Insert into registration(FirstName, LastName, Email, Password)
-      values(?, ?, ?, ?)");
-    $stmt->bind_param("ssss",$FirstName, $LastName, $Email, $Password);
+    $stmt = $conn->prepare("Insert into registration(UserName, Email, Password)
+      values(?, ?, ?)");
+    $stmt->bind_param("ssss",$UserName, $Email, $Password);
     $stmt->execute();
     echo "registration Successfully";
     $stmt->close();
