0391c288cbf6e87cf2b53f9e4c805a995ea7df20
Rolands <rolands_sapoznikovs@inbox.lv>
2022-12-11 21:01:33 +0200

    Adding MongoDB and Node.js to website

--- app.js
+++ app.js
@@ -1,26 +1,53 @@
 var express = require('express');   
+const { Server } = require('http');
 var app = express();
+var path = require('path');
+var mongoose = require('mongoose')
 
-app.use('/CSS', express.static('CSS'))
+mongoose.connect("mongodb+srv://Rolands:Rolands@fruitclicker.9yqkrai.mongodb.net/?retryWrites=true&w=majority")
 
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/index.html')
+var RegisterSchema = new mongoose.Schema({
+    Username: String,
+    Password: String,
+    Email: String,
 });
 
+var Register = mongoose.model('Register', RegisterSchema);
+var RegisterOne = Register({Username: 'Bob', Password: '123', Email: 'Bob@inbox.lv'}).save(function(err){
+    if(err) console.log(err);
+    console.log('item saved');
+});
+
+
+
+app.set('view engine', 'ejs');
+
+app.use('/CSS', express.static('CSS'))
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+app.get('/', function(req, res) {
+    res.render('index');
+});
+
+app.get('/index.html/', function(req, res){
+    res.render('index')
+})
+
 app.get('/login.html/', function(req, res) {
-    res.sendFile(__dirname + '/login.html')
+    res.render('login')
 });
 
 app.get('/register.html/', function(req, res) {
-    res.sendFile(__dirname + '/register.html')
+    res.render('register')
 });
 
 app.get('/reviews.html/', function(req, res) {
-    res.sendFile(__dirname + '/reviews.html')
+    res.render('reviews')
 });
 
 app.get('/update.html/', function(req, res) {
-    res.sendFile(__dirname + '/update.html')
+    res.render('update')
 });
 
 
