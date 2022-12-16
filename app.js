b3a16d8464e6f4fdc37ee028e4309172b7e59b5b
Rolands <rolands_sapoznikovs@inbox.lv>
2022-12-17 00:49:28 +0200

    Adding register errors

--- app.js
+++ app.js
@@ -6,17 +6,15 @@ var mongoose = require('mongoose');
 const { json } = require('body-parser');
 const req = require('express/lib/request');
 const User = require('./model/user')
-const bodyParser = require('body-parser');
-const { Console } = require('console');
-var jsonParser = bodyParser.json()
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
+const flash = require('connect-flash')
 const JWT_SECRET = 'faiwniwuefnuiwauefn348th3458tyh8dh&#G$&@#^$G&bfuywbuf'
 
 mongoose.connect("mongodb+srv://Rolands:Rolands@fruitclicker.9yqkrai.mongodb.net/?retryWrites=true&w=majority")
 
 
+app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs');
 
 app.use('/CSS', express.static('CSS'))
@@ -38,7 +36,7 @@ app.get('/login', function(req, res) {
 });
 
 app.get('/register', function(req, res) {
-    res.render('register')
+    res.render('register', {registerResponse: registerResponse})
 });
 
 app.get('/reviews', function(req, res) {
@@ -69,7 +67,7 @@ app.post('/login', async (req, res) => {
     if(await bcrypt.compare(regparoleinp, user.regparoleinp)) {
 
         const token = jwt.sign({ id: user._id, regemailinp: user.regemailinp},JWT_SECRET)
-        return res.json({status: 'ok', data: token})
+        return res.json({status: 'ok', data: token}), res.send
         
     }
 
@@ -77,46 +75,66 @@ app.post('/login', async (req, res) => {
 
 })
 
+let registerResponse = {status: '', error: ''};
 app.post('/register', async (req, res) =>{
 
      const {regusernameinp, regparoleinp: plainTextPassword, regemailinp} = req.body
 
-     if(!regusernameinp || typeof regusernameinp !== 'string') {
-        return res.json({status: 'error', error: 'Invalid Username'})
-     }
-
-     if(!plainTextPassword || typeof plainTextPassword !== 'string') {
-        return res.json({status: 'error', error: 'Invalid Password'})
-     }
-
-     if(!regemailinp || typeof regemailinp !== 'string') {
-        return res.json({status: 'error', error: 'Invalid Email'})
-     }
-
-     if(plainTextPassword.length < 5) {
-        return res.json({status: 'error', error: 'Password should be at least 6 characters long'})
-     }
-
-     const regparoleinp = await bcrypt.hash(plainTextPassword, 10)
-
-
-    try{
-        const response = await User.create({
-            regusernameinp,
-            regparoleinp,
-            regemailinp
-        })
-        console.log('User Created: ', response)
-    }catch(error){
-        console.log(JSON.stringify(error))
-         if(error.code === 11000) {
-            return res.json({status: 'error', error: 'Username already in use'})
-        }
-        throw error
-        
+     if (!regusernameinp || typeof regusernameinp !== 'string') {
+        // registerResponse = {status: 'error', error: 'Invalid Username'};
+        registerResponse.status = "error";
+        registerResponse.error = "Invalid Username";
+        // return res.render('pages/register', {registerResponse});
     }
 
-    return res.redirect('/login')
+    if (!plainTextPassword || typeof plainTextPassword !== 'string') {
+        registerResponse.status = "error";
+        registerResponse.error = "Invalid Password";
+        // return res.json({status: 'error', error: 'Invalid Password'})
+    }
+
+    if (!regemailinp || typeof regemailinp !== 'string') {
+        registerResponse.status = "error";
+        registerResponse.error = "Invalid Email";
+        // return res.json({status: 'error', error: 'Invalid Email'})
+    }
+
+    if (plainTextPassword.length < 5) {
+        registerResponse.status = "error";
+        registerResponse.error = "Password should be at least 6 characters long";
+        // return res.json({status: 'error', error: 'Password should be at least 6 characters long'})
+    }
+
+
+    if(registerResponse.status !== "error"){
+        const regparoleinp = await bcrypt.hash(plainTextPassword, 10)
+
+    
+        try {
+            const response = await User.create({
+                regusernameinp,
+                regparoleinp,
+                regemailinp
+            })
+            console.log('User Created: ', response);
+            registerResponse.status = "success";
+        } catch (error) {
+            console.log(error);
+            if (JSON.stringify(error.code === 11000)) {
+                // return res.json({status: 'error', error: 'Username already in use'})
+                registerResponse.status = "error";
+                registerResponse.error = "Username already in use";
+            }
+        }
+    }
+
+    if (registerResponse.status == "success") {
+        res.redirect('/login')
+    } else if (registerResponse.status == "error") {
+        res.redirect("/register")
+    } else {
+        console.log("Who this?")
+    }
  
     
 })
