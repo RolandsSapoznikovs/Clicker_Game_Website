9fc251afd44ffbbd5563eba892e13a0a00cda566
Rolands <rolands_sapoznikovs@inbox.lv>
2023-02-15 22:40:56 +0200

    Adding transitions, making show password work and fixing css for reviews

--- app.js
+++ app.js
@@ -44,6 +44,7 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs');
 
 app.use('/CSS', express.static('CSS'))
+app.use('/JS', express.static('JS'))
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.json())
 
